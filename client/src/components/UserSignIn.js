0fde9a78882bcab1d345cc29a4a3dcc5bc485c10
Snir Holland <sniro84@hotmail.com>
2019-09-29 16:51:25 +0300

    Update UserSignIn.js

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -21,6 +21,7 @@ class UserSignIn extends Component {
         e.preventDefault();
         const { context } = this.props;
         const { email, password } = this.state;
+       
     
         context.actions.signIn(email,password)
             .then( (userData) => {
